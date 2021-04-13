90bd7ad54d33d75ecf9aa84a3384a7759a13d1bd
Elouan <73824665+ByMSRT@users.noreply.github.com>
2021-04-13 09:53:23 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,26 +2,55 @@ package main
 
 import (
 	"html/template"
-	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
-var e *template.Template
-
-func accueil(w http.ResponseWriter, r *http.Request) {
-	e = template.Must(template.ParseFiles("templates/accueil.html"))
-}
-
-func handleRequests() {
-	router := mux.NewRouter().StrictSlash(true)
-
-	router.HandleFunc("/", accueil).Methods("GET")
-
-	log.Fatal(http.ListenAndServe(":8000", router))
-}
+var t *template.Template
 
 func main() {
-	handleRequests()
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	//http.HandleFunc("/", route)
+	http.HandleFunc("/", accueil)
+	http.HandleFunc("/test", test)
+	http.HandleFunc("/push", push)
+	http.ListenAndServe(":8000", nil)
+}
+
+func accueil(w http.ResponseWriter, r *http.Request) {
+	custTemplate, err := template.ParseFiles("./templates/accueil.html")
+
+	if err != nil {
+
+	}
+	err = custTemplate.Execute(w, nil)
+
+}
+
+/*func route(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" && r.URL.Path == "/test" {
+		http.Redirect(w, r, "test", http.StatusSeeOther)
+	} else if r.Method == "POST" && r.URL.Path == "/push" {
+		http.Redirect(w, r, "push", http.StatusSeeOther)
+	} else {
+		t.Execute(w, nil)
+	}
+}*/
+
+func test(w http.ResponseWriter, r *http.Request) {
+	custTemplate, err := template.ParseFiles("./templates/Test.html")
+
+	if err != nil {
+
+	}
+	err = custTemplate.Execute(w, nil)
+}
+
+func push(w http.ResponseWriter, r *http.Request) {
+	custTemplate, err := template.ParseFiles("./templates/push.html")
+
+	if err != nil {
+
+	}
+	err = custTemplate.Execute(w, nil)
 }
