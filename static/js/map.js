f75d0d0e0be0b55cfe9a071e886bb89e4def1f6b
t.bourmaud <tao.bourmaud@ynov.com>
2021-04-19 08:14:18 +0200

    des tests pour map

--- static/js/map.js
+++ static/js/map.js
@@ -1,13 +1,39 @@
+// function initMap() {
+//     const uluru = { lat: 47.205617056212475, lng: -1.539378717397206 };
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 4,
+//         center: uluru
+//     });
+
+//     const marker = new google.maps.Marker({
+//         position: uluru,
+//         map: map,
+//     });
+
+// }
+
 function initMap() {
-    const uluru = { lat: 47.205617056212475, lng: -1.539378717397206 };
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: uluru
+      zoom: 8,
+      center: { lat: -34.397, lng: 150.644 },
     });
-
-    const marker = new google.maps.Marker({
-        position: uluru,
-        map: map,
+    const geocoder = new google.maps.Geocoder();
+    document.getElementById("submit").addEventListener("click", () => {
+      geocodeAddress(geocoder, map);
     });
-
-}
+  }
+  
+  function geocodeAddress(geocoder, resultsMap) {
+    const address = document.getElementById("address").value;
+    geocoder.geocode({ address: address }, (results, status) => {
+      if (status === "OK") {
+        resultsMap.setCenter(results[0].geometry.location);
+        new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location,
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  }
