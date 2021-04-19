b95cdf5774dca813cfc8d5dd2407e48ec13ee16a
kevin <kevin.guyodo@ynov.com>
2021-04-19 15:08:06 +0200

    geolocation map

--- static/js/map.js
+++ static/js/map.js
@@ -1,13 +1,34 @@
 function initMap() {
-    const uluru = { lat: 47.205617056212475, lng: -1.539378717397206 };
+    // Définit une map qui pointe sur Ynov Nantes
+    const initPointer = { lat: 47.205389240386936, lng: -1.539540743860519 }
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: uluru
+        zoom: 8,
+        center: initPointer,
     });
+    const geocoder = new google.maps.Geocoder();
 
-    const marker = new google.maps.Marker({
-        position: uluru,
-        map: map,
+    // Evènement qui va pointer sur un lieu si il y a un click
+    document.getElementById("submit").addEventListener("click", () => {
+        geocodeAddress(geocoder, map);
+    });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+    const address = document.getElementById("address").value;
+
+    geocoder.geocode({ address: address }, (results, status) => {
+        // Si L'API google est on va changer les coordonnées GPS 
+        if (status === "OK") {
+            resultsMap.setCenter(results[0].geometry.location);
+            // On va recréer une map avec les nouvelles coordonnées GPS
+            new google.maps.Marker({
+            map: resultsMap,
+            position: results[0].geometry.location,
+        });
+        // Sinon message d'erreur 
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
     });
 
 }
