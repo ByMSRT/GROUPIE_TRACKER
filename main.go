04dd1c1b63404c70584ad530e7caa35bbfa6ee28
ByMSRT <73824665+ByMSRT@users.noreply.github.com>
2021-04-20 12:12:33 +0200

    rework system to get the api

--- main.go
+++ main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
 
 	controller "./controller"
 )
 
 func main() {
-
-	loadApi()
 
 	colorGreen := "\033[32m"
 	colorBlue := "\033[34m"
@@ -24,55 +19,16 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	//http.HandleFunc("/", route)
-	http.HandleFunc("/", controller.Accueil)
+
 	http.HandleFunc("/valider", controller.Test)
 	http.HandleFunc("/map", controller.Map)
 	http.HandleFunc("/search", controller.Search)
+	http.HandleFunc("/api/artists", controller.Artists)
+	http.HandleFunc("/api/locations", controller.Locations)
+	http.HandleFunc("/", controller.Accueil)
 
 	fmt.Println(string(colorGreen), "[SERVER_READY] : on http://localhost:8000 ✅ ")
 	fmt.Println(string(colorYellow), "[SERVER_INFO] : To stop the program : Ctrl + c")
 	http.ListenAndServe(":8000", nil)
 
-}
-
-type Response struct {
-	Id           int      `json:"id"`
-	Image        string   `json:"image"`
-	Name         string   `json:"name"`
-	Members      []string `json:"members"`
-	CreationDate uint16   `json:"creationDate"`
-	FirstAlbum   string   `json:"firstAlbum"`
-	Location     string   `json:"locations"`
-	ConcertDates string   `json:"concertDates"`
-	Relations    string   `json:"relations"`
-}
-
-func loadApi() {
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
-
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-
-	responseData, err := ioutil.ReadAll(response.Body)
-	/* var tabResponse = []byte(responseData) */
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	/* response1 := Response{}
-
-	test := json.Unmarshal(tabResponse, &response1)
-
-	if test != nil {
-		log.Fatal(test)
-	}
-	fmt.Println(response1.Id) */
-
-	fmt.Println(string(responseData))
-
-	/* var responseObject Response
-	json.Unmarshal(responseData, &responseObject)
-	fmt.Println(responseObject) */
 }
