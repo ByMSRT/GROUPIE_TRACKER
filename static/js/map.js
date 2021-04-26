37e1fca330e78ca21fb0770e1ff1ac0f566cced5
ByMSRT <73824665+ByMSRT@users.noreply.github.com>
2021-04-26 12:14:33 +0200

    update

--- static/js/map.js
+++ static/js/map.js
@@ -1,3 +1,6 @@
+let tabMarker = [];
+let marker;
+
 function initMap() {
     let initialPlace = { lat: 47.205389240386936, lng: -1.539540743860519 }
         // Définit une map qui pointe sur Ynov Nantes
@@ -6,35 +9,51 @@ function initMap() {
         center: initialPlace,
     });
 
-    new google.map.Marker({
+    marker = new google.maps.Marker({
         position: initialPlace,
-        title: document.getElementById("address").value
+        title: document.getElementById("search").value,
+        map: map
     })
 
+    tabMarker.push(marker)
     const geocoder = new google.maps.Geocoder();
 
     // Evènement qui va pointer sur un lieu si il y a un click
-    document.getElementById("submit").addEventListener("click", () => {
+    let newPlace = document.getElementById("submit")
+    newPlace.addEventListener("click", () => {
+        marker.setMap(null)
         geocodeAddress(geocoder, map);
     });
+
+    removeLastMarker()
 }
 
+
 function geocodeAddress(geocoder, resultsMap) {
-    const address = document.getElementById("address").value;
+    const address = document.getElementById("search").value;
 
     geocoder.geocode({ address: address }, (results, status) => {
         // Si L'API google est on va changer les coordonnées GPS 
         if (status === "OK") {
             resultsMap.setCenter(results[0].geometry.location);
             // On va recréer une map avec les nouvelles coordonnées GPS
-            new google.maps.Marker({
+            marker = new google.maps.Marker({
                 map: resultsMap,
                 position: results[0].geometry.location,
             });
+
             // Sinon message d'erreur 
         } else {
             alert("Error type :" + status);
         }
     });
 
+}
+
+// Fonction qui va retirer l'ancien marqueur 
+function removeLastMarker() {
+    if (tabMarker.length != 0) {
+        tabMarker = [];
+        marker.setMap(null)
+    }
 }
