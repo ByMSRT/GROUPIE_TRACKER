66e3a3c551c4295000c0f534da52dcfc4df0e691
t.bourmaud <tao.bourmaud@ynov.com>
2021-04-26 12:35:42 +0200

    début tri par date

--- static/js/accueil.js
+++ static/js/accueil.js
@@ -1,101 +1,95 @@
-// function MinAndMax(){
-//     var min = document.getElementById("date-min").value
-//     var max = document.getElementById("date-max").value
-//     if (min < 1950 || min > 2021 || max > 2021 || max < 1950){
-//         alert("Le minimum possible est 1950 et le maximum possible est 2021.")
-//     }
-
-//     for (let i = min; i < max ; i++) {
-//         if (creationdate = i ){
-
-//         }
-//     }
-
 const matchList = document.getElementById('card-data');
+
 const api = "/api/"
+
 const artist = "artists"
 const location_ = "locations"
+const date = "dates"
+const relation = "relation"
 
-const searchStates = async searchText => {
-    const res_artist = await fetch(api + artist);
-    const res_location = await fetch(api + location_);
-    const states = await res_artist.json();
-    const states2 = await res_location.json();
-    console.log(states2);
 
-    let matches = states.filter(state => {
-        const regex = new RegExp(`^${searchText}`, 'gi');
+async function data(url) {
+    const res_artist = await fetch(url);
 
-        let allMembers = ""
-
-        for (let index = 0; index < state.members.length; index++) {
-            allMembers += state.members[index]
-        }
-        console.log(allMembers)
-        let resultOfMatches = state.name.match(regex) || (state.creationDate).toString().match(regex) || allMembers.match(regex) || (state.firstAlbum).toString().match(regex)
-        return resultOfMatches
-
-    });
-    let matches2 = states2.index.filter(state2 => {
-        const regex = new RegExp(`^${searchText}`, 'gi');
-        return state2.locations[0].match(regex)
-    });
-    console.log(matches)
-    console.log(matches2)
-
-    if (searchText.length === 0) {
-        matches = [];
-        matches2 = [];
-        matchList.innerHTML = '';
+    const dataArtist = await res_artist.json();
+    console.log(dataArtist);
+    if (res_artist) {
+        hideloader();
     }
-    outputHtml(matches, matches2);
+    show(dataArtist);
 }
 
-const outputHtml = (matches, matches2) => {
+data(api + artist);
 
-    if (matches.length > 0) {
-        const html = matches.map(match => `
-        <div class="card" id="card">
-            <div class="card-header" id="card-header">
-                <img src="${match.image}" alt="">
-            </div>
-                <div class="card-body" id="card-body">
-                    <ul>
-                        <li><h4>Nom :</h4><br>${match.name}</li>
-                        <br>
-                        <li><h4>Date de création :</h4><br>${match.creationDate}</li>
-                        <br>
-                        <li><h4>Membres :</h4><br>${match.members}</li>
-                        <br>
-                        <li><h4>Premier album :</h4><br>${match.firstAlbum}</li>
-                    </ul>
-                    <div class="popup-header-cont">
-                        <h3>${match.name}</h3>
-                    </div>
-                    <div class="read-more-cont">
-                        <p>${match.locations}</p>
-                    </div>
-                    <button class="btn" type="button">Voir plus ...</button>
-                </div>
+function hideloader() {
+    document.getElementById('loading').style.display = 'none'
+}
+
+
+function show(dataArtist) {
+    var arr = []
+    const tab = dataArtist.map(match => `
+    <div class="card" id="card">
+        <div class="card-header" id="card-header">
+            <img src="${match.image}" alt="">
         </div>
-        
-        `).join('');
-
-        let finalhtml = html ;
-        matchList.innerHTML = finalhtml;
+            <div class="card-body" id="card-body">
+                <ul>
+                    <li><h4>Nom :</h4><br>${match.name}</li>
+                    <br>
+                    <li id="test" value="${match.creationDate}"><h4>Date de création :</h4><br>${match.creationDate}</li>
+                    <br>
+                    <li><h4>Membres :</h4><br>${match.members}</li>
+                    <br>
+                    <li><h4>Premier album :</h4><br>${match.firstAlbum}</li>
+                </ul>
+                <div class="popup-header-cont">
+                    <h3>${match.name}</h3>
+                </div>
+                <div class="read-more-cont">
+                </div>
+            <button class="btn" type="button">Voir plus ...</button>
+            </div>
+    </div>
+    
+    `);//.join('');
+    for (let i = mini; i <= maxi; i++){
+        if (match.creationDate == i){
+            arr.push(tab["l'index qui correspond"])
+        }
     }
+    arr.join('');
+    matchList.innerHTML = arr;
 }
 
 
 
+const cardData = document.querySelector(".row");
+const popup = document.querySelector(".popup-box");
+const popupCloseBtn = popup.querySelector(".popup-close-btn")
 
+cardData.addEventListener("click", function(event) {
+    if (event.target.tagName.toLowerCase() == "button") {
+        const item = event.target.parentElement;
+        const h3 = item.querySelector(".popup-header-cont").innerHTML;
+        const readMoreCont = item.querySelector(".read-more-cont").innerHTML;
+        popup.querySelector(".popup-header").innerHTML = h3;
+        popup.querySelector(".popup-body").innerHTML = readMoreCont
+        popupBox();
+    }
+})
 
+popupCloseBtn.addEventListener("click", popupBox);
 
+popup.addEventListener("click", function(event) {
+    if (event.target == popup) {
+        popupBox();
+    }
+})
 
-
-
-
-
+function popupBox() {
+    popup.classList.toggle("open");
+}
 
 var min = document.getElementById("mini")
 var max = document.getElementById("maxi")
@@ -104,7 +98,7 @@ function minOrMax(param) {
     let select = document.createElement("select")
     select.id = "Select"+param.id
     param.appendChild(select)
-
+    select.setAttribute("onchange", "MinAndMax()")
     if (param == min) {
         minimum(select)
     }
@@ -133,3 +127,13 @@ function maximum(param){
 
 minOrMax(min)
 minOrMax(max) 
+
+
+var mini
+var maxi 
+
+function MinAndMax(){
+    mini = document.getElementById("Selectmini").value
+    maxi = document.getElementById("Selectmaxi").value
+    console.log(mini)
+}
