c2354fbf27774ebc5bc3f4435e3145f4701f3400
MVERON <mathis.veron@ynov.com>
2021-04-26 11:00:10 +0200

    test

--- controller/controller.go
+++ controller/controller.go
@@ -1,85 +1,81 @@
 package controller
 
 import (
-	"fmt"
-	"html/template"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
+    "fmt"
+    "html/template"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
 )
 
 func Accueil(w http.ResponseWriter, r *http.Request) {
-	custTemplate, err := template.ParseFiles("./templates/accueilv2.html")
+    custTemplate, err := template.ParseFiles("./templates/accueilv2.html")
 
-	if err != nil {
+    if err != nil {
 
-	}
-	err = custTemplate.Execute(w, nil)
+    }
+    err = custTemplate.Execute(w, nil)
 }
 
 func Test(w http.ResponseWriter, r *http.Request) {
-	custTemplate, err := template.ParseFiles("./templates/test.html", "./templates/navigation.html")
+    custTemplate, err := template.ParseFiles("./templates/test.html", "./templates/navigation.html")
 
-	if err != nil {
+    if err != nil {
 
-	}
-	err = custTemplate.Execute(w, nil)
+    }
+    err = custTemplate.Execute(w, nil)
 }
 
 func Map(w http.ResponseWriter, r *http.Request) {
-	custTemplate, err := template.ParseFiles("./templates/map.html", "./templates/navigation.html")
-<<<<<<< HEAD
+    custTemplate, err := template.ParseFiles("./templates/map.html", "./templates/navigation.html")
 
-	if err != nil {
+    if err != nil {
 
-	}
-	err = custTemplate.Execute(w, nil)
+    }
+    err = custTemplate.Execute(w, nil)
 }
 
 func Search(w http.ResponseWriter, r *http.Request) {
-	custTemplate, err := template.ParseFiles("./templates/searchv2.html")
-=======
->>>>>>> 8671335c3f4c6096a4f996645d498fa7e1a20212
+    custTemplate, err := template.ParseFiles("./templates/searchv2.html")
 
-	if err != nil {
+    if err != nil {
 
-	}
-	err = custTemplate.Execute(w, nil)
-}
-
-func Search(w http.ResponseWriter, r *http.Request) {
-	custTemplate, err := template.ParseFiles("./templates/searchv2.html")
-
-	if err != nil {
-
-	}
-	err = custTemplate.Execute(w, nil)
+    }
+    err = custTemplate.Execute(w, nil)
 }
 
 func loadApi(w http.ResponseWriter, r *http.Request, endpoint string) {
 
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/" + endpoint)
+    response, err := http.Get("https://groupietrackers.herokuapp.com/api/" + endpoint)
 
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
+    if err != nil {
+        fmt.Print(err.Error())
+        os.Exit(1)
+    }
 
-	responseData, err := ioutil.ReadAll(response.Body)
+    responseData, err := ioutil.ReadAll(response.Body)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	w.Header().Add("Content-Type", "application/json")
-	w.Write(responseData)
+    w.Header().Add("Content-Type", "application/json")
+    w.Write(responseData)
 }
 
 func Artists(w http.ResponseWriter, r *http.Request) {
-	loadApi(w, r, "artists")
+    loadApi(w, r, "artists")
 }
 
 func Locations(w http.ResponseWriter, r *http.Request) {
-	loadApi(w, r, "locations")
+    loadApi(w, r, "locations")
+}
+
+func Dates(w http.ResponseWriter, r *http.Request) {
+    loadApi(w, r, "dates")
+}
+
+func Relation(w http.ResponseWriter, r *http.Request) {
+    loadApi(w, r, "relation")
 }
--- main.go
+++ main.go
@@ -1,37 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
 
-	controller "./controller"
+    controller "./controller"
 )
 
 func main() {
 
-	colorGreen := "\033[32m"
-	colorBlue := "\033[34m"
-	colorYellow := "\033[33m"
+    colorGreen := "\033[32m"
+    colorBlue := "\033[34m"
+    colorYellow := "\033[33m"
 
-	fmt.Println(string(colorBlue), "[SERVER_INFO] : Starting local Server...")
+    fmt.Println(string(colorBlue), "[SERVER_INFO] : Starting local Server...")
 
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+    fs := http.FileServer(http.Dir("static"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	//http.HandleFunc("/", route)
+    //http.HandleFunc("/", route)
 
-	http.HandleFunc("/valider", controller.Test)
-	http.HandleFunc("/map", controller.Map)
-	http.HandleFunc("/search", controller.Search)
-<<<<<<< HEAD
-=======
-	http.HandleFunc("/api/artists", controller.Artists)
-	http.HandleFunc("/api/locations", controller.Locations)
-	http.HandleFunc("/", controller.Accueil)
->>>>>>> 8671335c3f4c6096a4f996645d498fa7e1a20212
+    http.HandleFunc("/valider", controller.Test)
+    http.HandleFunc("/map", controller.Map)
+    http.HandleFunc("/search", controller.Search)
+    http.HandleFunc("/api/artists", controller.Artists)
+    http.HandleFunc("/api/locations", controller.Locations)
+    http.HandleFunc("/api/dates", controller.Dates)
+    http.HandleFunc("/api/relation", controller.Relation)
+    http.HandleFunc("/", controller.Accueil)
 
-	fmt.Println(string(colorGreen), "[SERVER_READY] : on http://localhost:8000 ✅ ")
-	fmt.Println(string(colorYellow), "[SERVER_INFO] : To stop the program : Ctrl + c")
-	http.ListenAndServe(":8000", nil)
+    fmt.Println(string(colorGreen), "[SERVER_READY] : on http://localhost:8000 ✅ ")
+    fmt.Println(string(colorYellow), "[SERVER_INFO] : To stop the program : Ctrl + c")
+    http.ListenAndServe(":8000", nil)
 
 }
