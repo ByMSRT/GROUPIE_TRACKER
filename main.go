148b20f68293b0af273135013d861d57a969e72d
MVERON <mathis.veron@ynov.com>
2021-04-27 11:37:53 +0200

    logo

--- main.go
+++ main.go
@@ -20,25 +20,12 @@ func main() {
 
     //http.HandleFunc("/", route)
 
-<<<<<<< HEAD
     http.HandleFunc("/valider", controller.Test)
     http.HandleFunc("/map", controller.Map)
     http.HandleFunc("/search", controller.Search)
     http.HandleFunc("/api/artists", controller.Artists)
     http.HandleFunc("/api/locations", controller.Locations)
-    http.HandleFunc("/api/dates", controller.Dates)
-    http.HandleFunc("/api/relation", controller.Relation)
     http.HandleFunc("/", controller.Accueil)
-=======
-	http.HandleFunc("/valider", controller.Test)
-	http.HandleFunc("/map", controller.Map)
-	http.HandleFunc("/search", controller.Search)
-	http.HandleFunc("/api/artists", controller.Artists)
-	http.HandleFunc("/api/locations", controller.Locations)
-	http.HandleFunc("/api/dates", controller.Dates)
-	http.HandleFunc("/api/relation", controller.Relation)
-	http.HandleFunc("/", controller.Accueil)
->>>>>>> 8d6c8c26761c37265601014a1570f1c9554ad893
 
     fmt.Println(string(colorGreen), "[SERVER_READY] : on http://localhost:8000 ✅ ")
     fmt.Println(string(colorYellow), "[SERVER_INFO] : To stop the program : Ctrl + c")
