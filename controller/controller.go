3d83c2103626cd3820c31d624d4d9313d33eddfe
ByMSRT <73824665+ByMSRT@users.noreply.github.com>
2021-05-04 11:03:57 +0200

    Update Error

--- controller/controller.go
+++ controller/controller.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
 	"strings"
 )
 
@@ -14,16 +12,19 @@ func Accueil(w http.ResponseWriter, r *http.Request) {
 	custTemplate, err := template.ParseFiles("./templates/accueilv2.html")
 
 	if err != nil {
-
+		codeErreur(w, r, 404)
+		return
 	}
+
 	err = custTemplate.Execute(w, nil)
 }
 
 func Map(w http.ResponseWriter, r *http.Request) {
-	custTemplate, err := template.ParseFiles("./templates/map.html", "./templates/navigation.html")
+	custTemplate, err := template.ParseFiles("./templates/map.html")
 
 	if err != nil {
-
+		codeErreur(w, r, 404)
+		return
 	}
 	err = custTemplate.Execute(w, nil)
 }
@@ -32,7 +33,8 @@ func Search(w http.ResponseWriter, r *http.Request) {
 	custTemplate, err := template.ParseFiles("./templates/searchv2.html")
 
 	if err != nil {
-
+		codeErreur(w, r, 404)
+		return
 	}
 	err = custTemplate.Execute(w, nil)
 }
@@ -42,14 +44,24 @@ func loadApi(w http.ResponseWriter, r *http.Request, endpoint string) {
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/" + endpoint)
 
 	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+		codeErreur(w, r, 500)
+		return
+	}
+
+	check := [4]string{"artists", "locations", "dates", "relation"}
+
+	for i := 0; i < len(check); i++ {
+		if endpoint != check[i] {
+			codeErreur(w, r, 500)
+			return
+		}
 	}
 
 	responseData, err := ioutil.ReadAll(response.Body)
 
 	if err != nil {
-		log.Fatal(err)
+		codeErreur(w, r, 500)
+		return
 	}
 
 	w.Header().Add("Content-Type", "application/json")
@@ -77,14 +89,15 @@ func getId(w http.ResponseWriter, r *http.Request, id string) {
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/relation/" + id)
 
 	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+		codeErreur(w, r, 500)
+		return
 	}
 
 	responseData, err := ioutil.ReadAll(response.Body)
 
 	if err != nil {
-		log.Fatal(err)
+		codeErreur(w, r, 500)
+		return
 	}
 
 	w.Header().Add("Content-Type", "application/json")
@@ -94,4 +107,23 @@ func getId(w http.ResponseWriter, r *http.Request, id string) {
 func RelationData(w http.ResponseWriter, r *http.Request) {
 	pathPart := strings.Split(r.URL.Path, "/")
 	getId(w, r, pathPart[len(pathPart)-1])
+}
+
+func codeErreur(w http.ResponseWriter, r *http.Request, status int) {
+
+	colorRed := "\033[31m"
+
+	if status == 404 {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		fmt.Println(string(colorRed), "[SERVER_ALERT] - 404 : File not found, or missing...")
+	}
+	if status == 400 {
+		http.Error(w, "400 Bad request", http.StatusBadRequest)
+		fmt.Println(string(colorRed), "[SERVER_ALERT] - 400 : Bad request")
+	}
+	if status == 500 {
+		http.Error(w, "500 Internal server error", http.StatusInternalServerError)
+		fmt.Println(string(colorRed), "[SERVER_ALERT] - 500 : Internal server error")
+	}
+
 }
